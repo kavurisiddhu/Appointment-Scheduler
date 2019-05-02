4656a9a67e7cf803dacd9d5c6baf49ba406bc628
kavurisiddhu <kavurisiddhu149@gmail.com>
2019-05-02 06:39:47 -0700

    Initial commit-  test module of the application..

---
+++ src/test/java/com/matrix/codechallenge/springboot/web/AppointmentSchedulerTest.java
@@ -0,0 +1,16 @@
+package com.matrix.codechallenge.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppointmentSchedulerTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
